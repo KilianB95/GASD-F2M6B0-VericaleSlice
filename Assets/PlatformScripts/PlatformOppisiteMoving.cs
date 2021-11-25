fbdd1e54dc8f0d1500cc23aa764c0a4f676537a8
rickdohmen <70955143+rickdohmen@users.noreply.github.com>
2021-11-25 12:13:18 +0100

    Moving Platforms

---
+++ Assets/PlatformScripts/PlatformOppisiteMoving.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformOppisiteMoving : MonoBehaviour
+{
+    private float useSpeed;
+    public float directionSpeed = 2.0f;
+    float origY;
+    public float distance = 2.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        origY = transform.position.y;
+        useSpeed = directionSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (origY - transform.position.y < -distance)
+        {
+            Debug.Log("loopt niet vast");
+            useSpeed = -directionSpeed;
+        }
+        
+        else if (origY - transform.position.y > distance)
+        {
+            Debug.Log("loopt vast");
+            useSpeed = directionSpeed;
+        }
+        transform.Translate(0, useSpeed * Time.deltaTime, 0);
+    }
+}
