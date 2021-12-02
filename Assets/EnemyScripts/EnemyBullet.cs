3282d3ec63068c7f2d92e0b0a41ff70a4314395f
rickdohmen <70955143+rickdohmen@users.noreply.github.com>
2021-12-02 15:09:04 +0100

    bezig met damage en heb heel even een test player health gemaakt om de damage uit te testen

--- Assets/EnemyScripts/EnemyBullet.cs
+++ Assets/EnemyScripts/EnemyBullet.cs
@@ -6,6 +6,17 @@ public class EnemyBullet : MonoBehaviour
 {
     public int damage = 3;
     public Rigidbody rb;
+    public GameObject Bullet;
+    
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Player")
+        {
+            //collision.gameObject.GetComponent<>();
+            Debug.Log("Taken damage");
+           
+        }
+    }
     
 }
