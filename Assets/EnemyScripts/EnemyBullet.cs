0798d377140efdf1a9911bea434fe258aad494c6
rickdohmen <70955143+rickdohmen@users.noreply.github.com>
2021-12-02 12:50:03 +0100

    2nd save commit en start van de bullet damage

---
+++ Assets/EnemyScripts/EnemyBullet.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public int damage = 3;
+    public Rigidbody rb;
+
+    
+}
