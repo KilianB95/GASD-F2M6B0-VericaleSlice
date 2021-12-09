be3ee56bcacfd322f52bb5ef6dcabb91b6614938
rickdohmen <70955143+rickdohmen@users.noreply.github.com>
2021-12-09 11:47:08 +0100

    code voor de ability is half af maar moet even nog kijken met een variable

--- Assets/GameplayScripts/OriBash.cs
+++ Assets/GameplayScripts/OriBash.cs
@@ -6,10 +6,31 @@ public class OriBash : MonoBehaviour
 {
     public float reachRadius = 1.35f;
     RaycastHit2D[] objectsNear;
+    Vector3 direction;
+    public float speed = 20;
+    public bool canBash;
+    GameObject BashableObj;
+    public float maxTime = 1f;
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    IEnumerator counter()
+    {
+        //yield return new WaitForSeconds()
+        float pauseTime = Time.realtimeSinceStartup + maxTime;
+
+        while (Time.realtimeSinceStartup<pauseTime)
+        {
+            yield return null;
+        }
+        if(Time.timeScale == 0)
+        {
+            Time.timeScale = 1f;
+            canBash = false;
+        }
     }
 
     // Update is called once per frame
@@ -23,14 +44,36 @@ public class OriBash : MonoBehaviour
                 {
                     if(obj.collider.gameObject.GetComponent<BashAble>() != null)
                     {
+                        BashableObj = obj.collider.gameObject;
+                        StartCoroutine("counter");
                         Time.timeScale = 0;
+
+                        canBash = true;
+                        
+                        break;
                     }
                 }
             }
         }
-        else if (Input.GetKeyDown(KeyCode.C))
+        else if (Input.GetKeyUp(KeyCode.C) && canBash)
         {
             Time.timeScale = 1;
+            direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - BashableObj.transform.position);
+            direction.z = 0;
+            direction = direction.normalized;
+
+            transform.position = BashableObj.transform.position + direction;
+
+            GetComponent<Movement>().outsideforce = true;
+            GetComponent<Rigidbody>().velocity = direction * speed;
+
+            canBash = false;
+        }
+
+        void onDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(transform.position, reachRadius);
         }
     }
 }
